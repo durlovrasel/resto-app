ca16a024ee57a6ce0d0ee788fdcba720526859e1
durlovrasel <raselahmeddurlov@gmail.com>
2022-11-15 18:54:50 +0600

    First Commit

---
+++ resources/views/thankyou.blade.php
@@ -0,0 +1,6 @@
+<x-guest-layout>
+    <div class="container w-full px-5 py-6 mx-auto">
+        <h1>Thank You!</h1>
+        <p>We have received your reservation request. We will get back to you soon.</p>
+      </div>
+</x-guest-layout>
